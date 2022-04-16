0d83f701fd957d839e64eeb5295236d0e0adc37c
Ibrahim Aliyev <100702986+ibrahimaliyevv2@users.noreply.github.com>
2022-04-16 21:27:20 +0400

    Update Program.cs

--- DelegateTask2/DelegateTask2/Program.cs
+++ DelegateTask2/DelegateTask2/Program.cs
@@ -7,31 +7,6 @@ namespace DelegateTask2
     {
       static void Main(string[] args)
         {
-            Student studen1 = new Student("Hikmet", 56);
-            Student studen2 = new Student("Abbas", 66);
-            Student studen3 = new Student("Nigar", 46);
-            Student studen4 = new Student("Rustem", 86);
-            Student studen5 = new Student("Tofiq", 96);
-
-            Group group = new Group();
-            group.Students.Add(studen1);
-            group.Students.Add(studen2);
-            group.Students.Add(studen3);
-            group.Students.Add(studen4);
-            group.Students.Add(studen5);
-
-            var wantedStudnets = group.Search(x => x.FullName.Length>5);
-            wantedStudnets = group.Search(delegate(Student std) { return std.Point > 65; });
-            wantedStudnets = group.Search(IsPassed);
-
-
-
-            foreach (var item in wantedStudnets)
-            {
-                Console.WriteLine(item.FullName+" - "+item.Point);
-            }
-
-
             Employee employee1 = new Employee
             {
                 Salary = 1000
