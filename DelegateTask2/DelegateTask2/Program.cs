2387880d2082a60d99337cbb73112b521db2ba30
Ibrahim Aliyev <100702986+ibrahimaliyevv2@users.noreply.github.com>
2022-04-16 21:24:39 +0400

    Update Program.cs

--- DelegateTask2/DelegateTask2/Program.cs
+++ DelegateTask2/DelegateTask2/Program.cs
@@ -5,28 +5,82 @@ namespace DelegateTask2
 {
     class Program
     {
-        static void Main(string[] args)
+      static void Main(string[] args)
         {
-            Employee employee1 = new Employee { Fullname = "Hikmet", Salary =  33 };
-            Employee employee2 = new Employee { Fullname = "Sabir", Salary = 120 };
-            Employee employee3 = new Employee { Fullname = "Huseyn", Salary = 123 };
+            Student studen1 = new Student("Hikmet", 56);
+            Student studen2 = new Student("Abbas", 66);
+            Student studen3 = new Student("Nigar", 46);
+            Student studen4 = new Student("Rustem", 86);
+            Student studen5 = new Student("Tofiq", 96);
+
+            Group group = new Group();
+            group.Students.Add(studen1);
+            group.Students.Add(studen2);
+            group.Students.Add(studen3);
+            group.Students.Add(studen4);
+            group.Students.Add(studen5);
+
+            var wantedStudnets = group.Search(x => x.FullName.Length>5);
+            wantedStudnets = group.Search(delegate(Student std) { return std.Point > 65; });
+            wantedStudnets = group.Search(IsPassed);
+
+
+
+            foreach (var item in wantedStudnets)
+            {
+                Console.WriteLine(item.FullName+" - "+item.Point);
+            }
+
+
+            Employee employee1 = new Employee
+            {
+                Salary = 1000
+            };
+            Employee employee2 = new Employee
+            {
+                Salary = 1500
+            };
+            Employee employee3 = new Employee
+            {
+                Salary = 2500
+            };
+            Employee employee4 = new Employee
+            {
+                Salary = 800
+            };
 
             Company company = new Company();
-            company.Emplo
+            company.Employees.Add(employee1);
+            company.Employees.Add(employee2);
+            company.Employees.Add(employee3);
+            company.Employees.Add(employee4);
 
-            List<Employee> result = company.FindAll(x=>x.Salary>100);
-            Employee employeeNeed = company.Find(x=>x.Fullname=="Hikmet");
-            bool check = company.Exists(x=>x.Salary<1000);
+            Console.WriteLine(company.Exists(x=>x.Salary>4000));
 
-            foreach (var item in result)
+            Console.WriteLine(company.Employees.Exists(x=>x.Salary>4000));
+
+            foreach (var item in company.Employees.FindAll(x=>x.Salary>1000))
+            {
+                Console.WriteLine(item.Salary);
+            }
+
+            List<int> nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            foreach (var item in nums.FindAll(delegate(int num) { return num % 2 == 0; }))
             {
                 Console.WriteLine(item);
             }
 
-            Console.WriteLine("====================");
-            Console.WriteLine(employeeNeed.Fullname + " " + employee1.Salary);
-            Console.WriteLine("====================");
-            Console.WriteLine(check);
+            var firstEvenNum = nums.Find(x => x % 2 == 0);
+            Console.WriteLine("firstEvenNum: "+firstEvenNum);
+
+            Console.WriteLine(nums.Exists(x=>x>5));
+
+        }
+
+        static bool IsPassed(Student student)
+        {
+            return student.Point > 50;
         }
     }
 }
